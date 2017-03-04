5c9e39d07c8d4572bd6a622f2f63db3cdfab3567
Keith Lawson <keithl.online@gmail.com>
2017-03-04 21:31:22 +0000

    mid ex7 mindbender

---
+++ ex7/findClosestCentroids.m
@@ -0,0 +1,53 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+L = size(X,1);
+
+% You neens Xd to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+%disp(size(X)); %300 2
+%disp(size(centroids)); %3 2
+%disp("!!");
+%X examples....
+for te = 1:L
+  %disp(te);
+  old_distance=999999999;
+ 
+  %centroids....
+  for ce = 1:K
+    %disp(ce);
+    
+    diff = X(te, :)'-centroids(ce, :)';
+    distance = sqrt(diff'*diff);
+    
+    if(distance < old_distance)
+      %fprintf('ASSIGNEMENT MADE.\n\n');
+      idx(te)=ce;
+      old_distance=distance;
+    endif
+    
+  endfor
+endfor
+
+
+
+% =============================================================
+
+end
+
